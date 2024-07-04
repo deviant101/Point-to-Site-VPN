71fbb9fa2b592cc235b76baeb1b184259ee0c29e
deviant101 <115878714+deviant101@users.noreply.github.com>
2024-07-04 16:33:27 +0500

    Public IP, Gatewaysub & VPN Gateway added

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "vpn-rg"
-  location = "central us"
+  name     = var.rg-name
+  location = var.region
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vpn-vnet"
+  name                = var.vnet-name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   address_space       = ["10.0.0.0/16"]
@@ -12,5 +12,36 @@ resource "azurerm_virtual_network" "vnet" {
   subnet {
     name           = "subnet"
     address_prefix = "10.0.1.0/24"
-  } 
+  }
+}
+
+resource "azurerm_subnet" "Gateway" {
+  name                 = "GatewaySubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "vpn-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Dynamic"
+
+}
+
+resource "azurerm_virtual_network_gateway" "vpn-gateway" {
+  name                = "vpn-gateway"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku  = "Basic"
+  type = "Vpn"
+
+  ip_configuration {
+    public_ip_address_id          = azurerm_public_ip.pip.id
+    subnet_id                     = azurerm_subnet.Gateway.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
 }
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "rg-name" {
+  default = "vpn-rg"
+}
+
+variable "region" {
+  default = "central us"
+}
+
+variable "vnet-name" {
+  default = "vpn-vnet"
+}
